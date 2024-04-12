4b4f981997d5136ad18c980b57a36d18122d9ef7
BruceZhu <276423059@qq.com>
2024-04-12 16:54:44 +0800

    version updated

---
+++ podTrunk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+
+#Verify variable is provided
+if [ "$1" = "" ]; then
+        echo -e "Version number not provide"
+        exit 1
+fi
+
+VERSION=$1
+
+cd ~/ble-sdk-1/HeloBleLib/Example/HeloBleLib
+sed -i "" "s/\"\([0-9]\)\.\([0-9]\)\.\([0-9]\)/\"${VERSION}/g" HeloBleLib.podspec
+git add .
+git commit -am "${VERSION}" 
+git push
+git tag ${VERSION}
+git push --tags
+pod lib lint --allow-warnings
+pod trunk push HeloBleLib.podspec
