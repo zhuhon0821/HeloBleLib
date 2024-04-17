f5c39799e91674423bcba4fda510bceab071ff39
BruceZhu <276423059@qq.com>
2024-04-17 15:19:58 +0800

    demo 后台任务执行

--- Example/HeloBleLib/AppDelegate.swift
+++ Example/HeloBleLib/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var backgroundTaskIdentifier:UIBackgroundTaskIdentifier?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow()
@@ -31,6 +31,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask {[weak self]in
+            if let id = self?.backgroundTaskIdentifier {
+                UIApplication.shared.endBackgroundTask(id)
+            }
+            
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
